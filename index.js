79ebfe765bb84a677569fcd16c7fd25cbf981750
Coby Shelton <cobyshelton@cobys-air.mynetworksettings.com>
2026-07-05 13:59:26 -0500

    commissioned

---
+++ index.js
@@ -0,0 +1,11 @@
+function ministryDescription() {
+
+}
+
+function openMenu(){
+    document.body.classList += "menu--open"
+}
+
+function closeMenu(){
+    document.body.classList.remove('menu--open')
+}
