4a9ad2e1a7075d08c1f16911e0f134d9e173bd1d
Coby Shelton <cobyshelton@cobys-air.mynetworksettings.com>
2026-07-20 07:24:53 -0500

    6

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const backdrop = document.getElementById('menuBackdrop');
 const openButton = document.getElementById('openMenu');
 const closeButton = document.getElementById('closeMenu');
+const menuLinks = document.querySelectorAll(".menu__link")
 
 function toggleMenu() {
-    backdrop.classList.toggle('menu--open');
+    document.body.classList.toggle('menu--open');
 }
 
 openButton.addEventListener('click', toggleMenu);
 closeButton.addEventListener('click', toggleMenu);
+
+menuLinks.forEach{
+    addEventListener('click', toggleMenu)
+};
